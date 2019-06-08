adcd3ad2fbf7a3eab6b8ce8f6da1958a85ce3d16
binsu <binsu.cs@qq.com>
2019-06-08 15:39:56 +0800

    update

--- d-vector.py
+++ d-vector.py
@@ -8,7 +8,6 @@ import json
 import numpy as np
 import tensorflow as tf
 from tensorflow.data import Dataset
-from models import *
 from data_loader import load_data, build_data
 from layers import Maxout
 
@@ -47,77 +46,92 @@ def cnn_model(inputs):
 
 def create(config):
     global_step = tf.Variable(0, name='global_step')
-    drop_rate = tf.placeholder(tf.float32, shape=[1], name='drop_rate')
+    drop_rate = tf.placeholder(tf.float32, shape=None, name='drop_rate')
 
     inputs = tf.placeholder(dtype=tf.float64, shape=[None, 1640], name='x')
     labels = tf.placeholder(dtype=tf.int64, shape=[None], name='y')
-    inner_labels = tf.reshape(labels, [-1])
     if config['model'] == 'maxout':
         with tf.variable_scope('maxout'):
             net = maxout_model(inputs, drop_rate)
     elif config['model'] == 'dnn':
         with tf.variable_scope('dnn'):
             net = dnn_model(inputs)
-    logits = tf.layers.Dense(int(config['speaker_number']))(net, name='logits')
+    logits = tf.layers.Dense(int(config['speaker_number']),name='logits')(net)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    opti = tf.train.AdagradOptimizer(0.01).minimize(loss,global_step=global_step,name='optimizer')
+    loss_summary=tf.summary.scalar('loss',loss)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(init)
-        saver.save(sess, config['model_name'], global_step=global_step)
+        saver.save(sess, config['model_name'])
 
 
 def train(config, train_set, test_set):
     train_iter = build_data(
-        train_set, BATCH, repeat=True).make_one_shot_iterator().get_next()
+        train_set, int(config['batch']), repeat=True).make_one_shot_iterator().get_next()
     dev_iter = build_data(
-        dev_set, BATCH, repeat=True).make_one_shot_iterator().get_next()
+        dev_set, int(config['batch']), repeat=True).make_one_shot_iterator().get_next()
 
     model = tf.train.get_checkpoint_state(config['model_dir'])
-    saver = tf.train.import_meta_graph(model.model_checkpoint_path+'.meta')
+    saver = tf.train.import_meta_graph(config['model_name']+'.meta')
     graph = tf.get_default_graph()
-    inputs = graph.get_tensor_by_name('inputs')
-    labels = graph.get_tensor_by_name('labels')
-    global_step = graph.get_tensor_by_name('global_step')
-    drop_rate = graph.get_tensor_by_name('drop_rate')
-    logits = graph.get_tensor_by_name('logits')
+    inputs = graph.get_tensor_by_name('x:0')
+    labels = graph.get_tensor_by_name('y:0')
+    global_step = graph.get_tensor_by_name('global_step:0')
+    drop_rate = graph.get_tensor_by_name('drop_rate:0')
+    logits = graph.get_tensor_by_name('logits/BiasAdd:0')
+    loss_summary=graph.get_tensor_by_name('loss:0')
+    opti=graph.get_tensor_by_name('optimizer:0')
 
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    opti = tf.train.AdamOptimizer(0.01).minimize(loss)
+    predictions=tf.argmax(logits,1)
     accuracy = tf.reduce_mean(
-        tf.equal(labels-tf.argmax(logits, 1)))
+        tf.cast(tf.equal(labels,predictions),tf.float32))
     stream_accuracy = tf.metrics.accuracy(
-        labels=labels, logits=logits, name='stream_accuracy')
+        labels=labels, predictions=predictions, name='stream_accuracy')
     stream_accuracy_init = tf.variables_initializer(var_list=tf.get_collection(
         tf.GraphKeys.LOCAL_VARIABLES, scope='stream_accuracy'))
+    init=tf.global_variables_initializer()
 
-    tf.summary.scalar('loss', loss)
-    tf.summary.scalar('accuracy', accuracy)
-    tf.summary.scalar('accuracy', stream_accuracy)
-    summary = tf.summary.merge_all()
+    batch_acc_summary=tf.summary.scalar('accuracy',accuracy)
+    stream_acc_summary=tf.summary.scalar('global_accuracy',stream_accuracy[1])
+    train_summary=tf.summary.merge(
+        inputs=[
+            loss_summary,batch_acc_summary
+        ]
+    )
+    dev_summary=tf.summary.merge(
+        inputs=[
+            stream_acc_summary,
+        ]
+    )
 
     train_writer = tf.summary.FileWriter(config['train_dir'])
     dev_writer = tf.summary.FileWriter(config['dev_dir'])
 
     with tf.Session() as sess:
+        sess.run(init)
         saver.restore(sess, model.model_checkpoint_path)
 
         for epoch in range(int(config['epoch'])):
             for i in range(int(config['speaker_number'])*100):
                 x, y = sess.run(train_iter)
-                result = sess.run([opti, accuarcy, summary], feed_dict={
+                y=np.reshape(y,[-1])
+                result = sess.run([opti,train_summary], feed_dict={
                     inputs: x, labels: y, drop_rate: 0.5
                 })
-                train_writer.add_summary(res[-1], global_step=global_step.eval())
+                train_writer.add_summary(result[-1], global_step=global_step.eval())
 
             sess.run(stream_accuracy_init)
             for i in range(int(config['speaker_number'])*10):
                 x, y = sess.run(dev_iter)
-                result = sess.run([stream_accuracy, summary], feed_dict={
+                y=np.reshape(y,[-1])
+                result = sess.run([dev_summary], feed_dict={
                     inputs: x, labels: y, drop_rate: 0
                 })
-            dev_writer.add_summary(res[-1],global_step=global_step.eval())
-        saver.save(sess,config['model_name'],global_step=global_step)
+            dev_writer.add_summary(result[-1],global_step=global_step.eval())
+        saver.save(sess,config['model_name'],global_step=global_step,write_meta_graph=False)
 
 
 if __name__ == '__main__':
@@ -127,13 +141,13 @@ if __name__ == '__main__':
         config = json.load(f)
 
     train_set, dev_set, test_set = load_data(config['src'],
-                                             config['speaker_number'],
-                                             config['dev_rate'],
-                                             config['test_rate'])
+                                             int(config['speaker_number']),
+                                             float(config['dev_rate']),
+                                             float(config['test_rate']))
     if cmd == 'create':
         create(config)
     elif cmd == 'train':
-        train(config, train_set, dev_test)
+        train(config, train_set, dev_set)
     elif cmd == 'test':
         test(config)
     else:
